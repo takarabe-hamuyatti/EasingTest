911cd1e433a3532cc6bb2dea501674a462cd7869
hamuyatti <syota.takarabe@gmail.com>
2022-11-17 00:35:42 +0900

    修正

--- app/src/main/java/com/example/easingtest/Adapter.kt
+++ app/src/main/java/com/example/easingtest/Adapter.kt
@@ -10,14 +10,17 @@ class Adapter : GroupieAdapter() {
                 itemList.forEach {
                     add(Item(it))
                 }
-                itemList.forEach {
-                    add(Item(it))
-                }
             }
         )
     }
 
     private val itemList = listOf(
+        R.drawable.image1,
+        R.drawable.image2,
+        R.drawable.image3,
+        R.drawable.image4,
+        R.drawable.image5,
+        R.drawable.image6,
         R.drawable.image1,
         R.drawable.image2,
         R.drawable.image3,
