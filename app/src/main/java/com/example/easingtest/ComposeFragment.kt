b9209619060fb357f1edff10ff4a6889f65df3ff
hamuyatti <syota.takarabe@gmail.com>
2022-12-29 05:00:27 +0900

    compose導入

---
+++ app/src/main/java/com/example/easingtest/ComposeFragment.kt
@@ -0,0 +1,17 @@
+package com.example.easingtest
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ComposeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
